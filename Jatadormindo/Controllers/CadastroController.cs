bc0cb3e4d7f636dda33c2aa6c11b88665a7523c0
Raythan Padovani Abreu Machado <33856352+Raythan@users.noreply.github.com>
2021-11-25 23:42:14 -0300

    alterações

--- Jatadormindo/Controllers/CadastroController.cs
+++ Jatadormindo/Controllers/CadastroController.cs
@@ -17,6 +17,12 @@ namespace Jatadormindo.Controllers
         }
         public ActionResult Cadastrar(Cadastro model)
         {
+            using (SQLConnect connect = new SQLConnect())
+            {
+                //NOME, EMAIL, SENHA, ENDERECO, CIDADE, ESTADO, CEP, CPF, IDADE
+                connect.InsereCadastro("INSERT INTO CADASTROS (NOME, EMAIL, ENDERECO, CIDADE, ESTADO, CEP, CPF, IDADE) " + 
+                    $"VALUES('{model.Nome}', '{model.Email}', '{model.Endereco}', '{model.Cidade}', '{model.Estado}', {model.CEP}, {model.CPF}, {model.Idade}) ");
+            }
             return View("Index");
         }
     }
--- Jatadormindo/Models/SQLConnect.cs
+++ Jatadormindo/Models/SQLConnect.cs
@@ -1,11 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
 namespace Jatadormindo.Models
 {
-    public class Class1
+    public class SQLConnect : IDisposable
     {
+        private string connectionstr = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=TrabalhoEntrega;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        private SqlConnection conn;
+        public void InsereCadastro(string qry)
+        {
+            conn.Open();
+            SqlTransaction trans = conn.BeginTransaction();
+            try
+            {
+                SqlCommand com = new SqlCommand(qry, conn, trans)
+                {
+                    CommandType = System.Data.CommandType.Text,
+                    CommandTimeout = 30
+                };
+                com.ExecuteNonQuery();
+                trans.Commit();
+            }
+            catch (Exception ex)
+            {
+                trans.Rollback();
+            }
+        }
+        public void Dispose()
+        {
+            if(conn.State != System.Data.ConnectionState.Closed)
+                conn.Close();
+        }
+        public SQLConnect() => conn = new SqlConnection(connectionstr);
     }
 }
